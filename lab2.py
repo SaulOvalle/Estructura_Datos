ee439e58208a34476f88678e32d2f868b9539a2b
unknown <saovallem@correo.edu.gt>
2023-04-30 04:31:27 -0600

    Funcion del tipo de construccion

--- lab2.py
+++ lab2.py
@@ -1,1 +1,13 @@
-print ("esta solo es para ver si si llega a sevir esta cosa solo la voy a borrar despues")
+import json as jreader
+
+def ReadJsonFile():
+    data = []
+    with open (r'C:\Users\Saul\Downloads\input_challenge_lab_2.jsonl') as JsonFile:
+        for line in jsonFile:
+            data.append(json.loads(line))
+    return data
+
+#Leemos la informacion y aplicamos el filtro por cada espacio
+data = jreader.ReadJsonFile()
+for i in range(len(data)):
+    pp.filterMatchPlaces(data[i]['input1'],data[i]['input2'])
